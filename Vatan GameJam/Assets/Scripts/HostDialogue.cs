96b96714664a53eb3268b0ed4f7a03532a791e97
kayra <khayridural@gmail.com>
2022-09-09 02:48:17 +0300

    ad

--- Vatan GameJam/Assets/Scripts/HostDialogue.cs
+++ Vatan GameJam/Assets/Scripts/HostDialogue.cs
@@ -5,9 +5,15 @@ using TMPro;
 
 public class HostDialogue : MonoBehaviour //evet hardcode ama napim sýkýldým
 {
+    [SerializeField] GameObject GoToDisable;
+    [SerializeField] GameObject GoToEnable;
     [SerializeField] TextMeshProUGUI textMesh;
+    [SerializeField] TextMeshProUGUI textMeshAfterDestruction;
+    [SerializeField] TextMeshProUGUI textMeshScoreBoard;
     [SerializeField] TextMeshProUGUI NumTakerDigitalText;
+    [SerializeField] Camera cameraToTurn;
     [SerializeField] float nextInputWaitTime = .2f;
+    [SerializeField] [Range(1, 100)] int[] tresholds = new int[] { 90, 75, 60, 45, 30, 15 };
     [SerializeField] GameObject NumTaker;
  
     Queue<string> DialogueQueue = new Queue<string>();
@@ -18,14 +24,19 @@ public class HostDialogue : MonoBehaviour //evet hardcode ama napim sýkýldým
     string[][] Features;
     int[] Prices;
 
+    int[] PlayerAnswers;
+
     bool lockAnswer;
     bool acceptAnswer;
     int currentAnswer = -1;
-    int pricesIndex;
+    int playerAnswerIndex;
+    bool turnCAM;
     bool OneTimeEnterPass;
 
     void Start()
     {
+        PlayerAnswers = new int[Prices.Length];
+
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
 
@@ -70,9 +81,11 @@ public class HostDialogue : MonoBehaviour //evet hardcode ama napim sýkýldým
                 DialogueQueue.Enqueue("So... What's it's price?<br>(Click then give your answer)");
                 DialogueQueue.Enqueue("(a " + Types[i].ToString() + " with " + sentence + ")<GetAnswer>");//<GetAnswer> aþaðýda updatede siliniyo ve cevap isteniyo
 
-                DialogueQueue.Enqueue("You chose your answer to be <CurrentAnswer>TL.");
+                DialogueQueue.Enqueue("You chose your answer to be <CurrentAnswer>TL. The correct answer was <CorrectAnswer>TL.");
             }
             DialogueQueue.Enqueue("And that concludes your turn!");
+            DialogueQueue.Enqueue("Lets see your score!<br>(Press E or LMB to see your score)");
+            DialogueQueue.Enqueue("<GoToScoreboard>");
 
             textMesh.text = DialogueQueue.Dequeue();
             cooldown = Time.time;
@@ -106,16 +119,63 @@ public class HostDialogue : MonoBehaviour //evet hardcode ama napim sýkýldým
                         currentAnswer = -1;
                         NumTaker.SetActive(true);
                     }
-                    else if(sentence.Contains("<CurrentAnswer>") && Prices.Length > pricesIndex - 1)
+                    if(sentence.Contains("<CurrentAnswer>") && PlayerAnswers.Length > playerAnswerIndex - 1)
                     {
-                        sentence = sentence.Replace("<CurrentAnswer>", Prices[pricesIndex - 1].ToString());
+                        sentence = sentence.Replace("<CurrentAnswer>", PlayerAnswers[playerAnswerIndex - 1].ToString());
+                    }
+                    if(sentence.Contains("<CorrectAnswer>"))
+                    {
+                        sentence = sentence.Replace("<CorrectAnswer>", Prices[playerAnswerIndex - 1].ToString());
+                    }
+                    if (sentence.Contains("<GoToScoreboard>"))
+                    {
+                        sentence = sentence.Replace("<GoToScoreboard>", "");
+
+                        int MaxScore = 0;
+                        foreach (int i in Prices)
+                            MaxScore += i;
+
+                        int perc = 0;
+                        for(int i = 0; i < PlayerAnswers.Length; i++)
+                        {
+                            perc += Mathf.RoundToInt(Mathf.Abs(PlayerAnswers[i] - Prices[i]) / Prices[i] * 100);
+                            Debug.Log(perc);
+                        }
+                        perc /= PlayerAnswers.Length;
+                        int PlayerScore = MaxScore * perc;
+
+                        textMeshScoreBoard.text = "<align=\"center\"><size=120%>SCORE BOARD<br><size=100%></align><br>";
+
+                        List<string> names = new List<string>() { "Kayra     ", "Murat     ", "Utku      ", "Berke     ", "Jamal     ", "Ecem      "};
+                        bool nameIsUsed = false;
+                        int yourPlace = 0;
+                        for(int i = 0; i < tresholds.Length; i++)
+                        {
+                            if (PlayerScore >= MaxScore * tresholds[i] / 100 && !nameIsUsed)
+                            {
+                                nameIsUsed = true;
+                                yourPlace = i + 1;
+                                textMeshScoreBoard.text += $"<color=\"red\">YOU<color=\"white\">       with {PlayerScore} score<br>";
+                            }
+
+                            int randomInt = Random.Range(0, tresholds.Length - i);
+                            textMeshScoreBoard.text += names[randomInt] + $"with {MaxScore * tresholds[i] / 100} score<br>";
+                            names.RemoveAt(randomInt);
+                        }
+
+                        if(PlayerScore < MaxScore * tresholds[5] / 100)
+                        {
+                            textMeshScoreBoard.text += $"<color=\"red\"> YOU<color=\"white\">        with {PlayerScore} score<br>";
+                            yourPlace = 7;
+                        }
+                        textMeshAfterDestruction.text = $"You are number {yourPlace} out of 7.";
+
+                        GoToDisable.SetActive(false);
+                        GoToEnable.SetActive(true);
+                        lockAnswer = true;
                     }
                     textMesh.text = sentence;
                     cooldown = Time.time + nextInputWaitTime;
-                }
-                else
-                {
-                    //bitir lan hakem BÝTÝÝÝR ÇAL DÜDÜÐÜ HAKEEEEEEEEEAAM
                 }
             }
         }
@@ -167,8 +227,8 @@ public class HostDialogue : MonoBehaviour //evet hardcode ama napim sýkýldým
     {
         if (currentAnswer >= 0 && currentAnswer < 100000 && acceptAnswer)
         {
-            Prices[pricesIndex] = currentAnswer;
-            pricesIndex++;
+            PlayerAnswers[playerAnswerIndex] = currentAnswer;
+            playerAnswerIndex++;
 
             lockAnswer = false;
             acceptAnswer = false;
@@ -182,6 +242,6 @@ public class HostDialogue : MonoBehaviour //evet hardcode ama napim sýkýldým
         Types = types;
         Names = names;
         Features = features;
-        Prices = new int[prices.Length];
+        Prices = prices;
     }
 }
