022cae954d806c0440ebb3f0095762a907cc132b
kayra <khayridural@gmail.com>
2022-09-08 20:56:18 +0300

    asd

--- Vatan GameJam/Assets/Scripts/HostDialogue.cs
+++ Vatan GameJam/Assets/Scripts/HostDialogue.cs
@@ -20,59 +20,59 @@ public class HostDialogue : MonoBehaviour //evet hardcode ama napim sýkýldým
 
     void Start()
     {
-        DialogueQueue.Enqueue("And now ladies and gentlemen we are here with our last contestant.");
-        DialogueQueue.Enqueue("Our dear contestant, are you ready?");
-        DialogueQueue.Enqueue("(Press E or LMB to start the competition)");
-
-        DialogueQueue.Enqueue("Alright then, here is your first product!");
-
-        for (int i = 0; i < Types.Length;i++)
+        if (Types != null)
         {
-            string sentence = "";
+            DialogueQueue.Enqueue("And now ladies and gentlemen we are here with our last contestant.");
+            DialogueQueue.Enqueue("Our dear contestant, are you ready?");
+            DialogueQueue.Enqueue("(Press E or LMB to start the competition)");
 
-            if (i == 0)
-                sentence = "First we have a ";
-            else if (i == Types.Length - 1)
-                sentence = "Lastly we have a ";
-            else
-                sentence = "Next is a ";
+            DialogueQueue.Enqueue("Alright then, here is your first product!");
 
-            sentence += Types[i] + $", {Names[i]}.";
-
-            DialogueQueue.Enqueue(sentence);
-
-            DialogueQueue.Enqueue("It's features are like this:");
-
-            sentence = "";
-            for(int n = 0; n < Features[i].Length; n++)
+            for (int i = 0; i < Types.Length; i++)
             {
-                sentence += Features[i][n];
+                string sentence = "";
 
-                if (n == Features[i].Length - 2)//son özellikten bi önce
-                    sentence += " and ";
-                else if (n == Features[i].Length - 1)//son özellik
-                    sentence += ".";
+                if (i == 0)
+                    sentence = "First we have a ";
+                else if (i == Types.Length - 1)
+                    sentence = "Lastly we have a ";
                 else
-                    sentence += ", ";
+                    sentence = "Next is a ";
+
+                sentence += Types[i] + $", {Names[i]}.";
+
+                DialogueQueue.Enqueue(sentence);
+
+                DialogueQueue.Enqueue("It's features are like this:");
+
+                sentence = "";
+                for (int n = 0; n < Features[i].Length; n++)
+                {
+                    sentence += Features[i][n];
+
+                    if (n == Features[i].Length - 2)//son özellikten bi önce
+                        sentence += " and ";
+                    else if (n == Features[i].Length - 1)//son özellik
+                        sentence += ".";
+                    else
+                        sentence += ", ";
+                }
+                DialogueQueue.Enqueue(sentence);
+                DialogueQueue.Enqueue("So... What's it's price?<br>(Click then give your answer)");
+                DialogueQueue.Enqueue("(a " + Types[i].ToString() + " with " + sentence + ")<GetAnswer>");//<GetAnswer> aþaðýda updatede siliniyo ve cevap isteniyo
+
+                DialogueQueue.Enqueue("You gave your answer.");
             }
-            DialogueQueue.Enqueue(sentence);
-            DialogueQueue.Enqueue("So... What's it's price?<br>(Click then give your answer)");
-            DialogueQueue.Enqueue("(a " + Types[i].ToString() + " with " + sentence + ")");
+            DialogueQueue.Enqueue("And that concludes your turn!");
 
-            //fiyatý vericek
-
-            DialogueQueue.Enqueue("You gave your answer.");
+            textMesh.text = DialogueQueue.Dequeue();
+            cooldown = Time.time;
         }
-        DialogueQueue.Enqueue("And that concludes your turn!");
-
-        textMesh.text = DialogueQueue.Dequeue();
-        cooldown = Time.time;
-
-        //int number = DialogueQueue.Count;
-        //for (int i = 0; i < number; i++)
-        //{
-        //    Debug.Log(DialogueQueue.Dequeue());
-        //}
+        else
+        {
+            textMesh.text = "I'm sorry my dear audience but it seems our staff forgot to select items. >:(";
+            Debug.Log("<ERROR - No selected object found...>");
+        }
     }
 
     void Update()
@@ -83,7 +83,17 @@ public class HostDialogue : MonoBehaviour //evet hardcode ama napim sýkýldým
             {
                 if (DialogueQueue.Count > 0)
                 {
-                    textMesh.text = DialogueQueue.Dequeue();
+                    string sentence = "";
+                    sentence = DialogueQueue.Dequeue();
+
+                    if (sentence.Contains("<GetAnswer>"))
+                    {
+                        sentence = sentence.Replace("<GetAnswer>", "");
+
+                        lockAnswer = true;
+                        //cevap iste
+                    }
+                    textMesh.text = sentence;
                     cooldown = Time.time + nextInputWaitTime;
                 }
             }
