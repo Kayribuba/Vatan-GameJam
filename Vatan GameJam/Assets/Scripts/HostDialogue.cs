cc63b90e780db34dbef64feecf1b7f4ccbc9631d
kayra <khayridural@gmail.com>
2022-09-08 19:45:28 +0300

    push

--- Vatan GameJam/Assets/Scripts/HostDialogue.cs
+++ Vatan GameJam/Assets/Scripts/HostDialogue.cs
@@ -7,9 +7,16 @@ public class HostDialogue : MonoBehaviour //evet hardcode ama napim sýkýldým
 {
     [SerializeField] TextMeshProUGUI textMesh;
     [SerializeField] float nextInputWaitTime = .2f;
-
+ 
     Queue<string> DialogueQueue = new Queue<string>();
     float cooldown = float.MaxValue;
+
+    string[] Types;
+    string[] Names;
+    string[][] Features;
+    int[] Prices;
+
+    bool lockAnswer;
 
     void Start()
     {
@@ -17,6 +24,77 @@ public class HostDialogue : MonoBehaviour //evet hardcode ama napim sýkýldým
         DialogueQueue.Enqueue("Our dear contestant, are you ready?");
         DialogueQueue.Enqueue("(Press E or LMB to start the competition)");
 
-        
+        DialogueQueue.Enqueue("Alright then, here is your first product!");
+
+        for (int i = 0; i < Types.Length;i++)
+        {
+            string sentence = "";
+
+            if (i == 0)
+                sentence = "First we have a ";
+            else if (i == Types.Length - 1)
+                sentence = "Lastly we have a ";
+            else
+                sentence = "Next is a ";
+
+            sentence += Types[i] + $", {Names[i]}.";
+
+            DialogueQueue.Enqueue(sentence);
+
+            DialogueQueue.Enqueue("It's features are like this:");
+
+            sentence = "";
+            for(int n = 0; n < Features[i].Length; n++)
+            {
+                sentence += Features[i][n];
+
+                if (n == Features[i].Length - 2)//son özellikten bi önce
+                    sentence += " and ";
+                else if (n == Features[i].Length - 1)//son özellik
+                    sentence += ".";
+                else
+                    sentence += ", ";
+            }
+            DialogueQueue.Enqueue(sentence);
+            DialogueQueue.Enqueue("So... What's it's price?<br>(Click then give your answer)");
+            DialogueQueue.Enqueue("(a " + Types[i].ToString() + " with " + sentence + ")");
+
+            //fiyatý vericek
+
+            DialogueQueue.Enqueue("You gave your answer.");
+        }
+        DialogueQueue.Enqueue("And that concludes your turn!");
+
+        textMesh.text = DialogueQueue.Dequeue();
+        cooldown = Time.time;
+
+        //int number = DialogueQueue.Count;
+        //for (int i = 0; i < number; i++)
+        //{
+        //    Debug.Log(DialogueQueue.Dequeue());
+        //}
+    }
+
+    void Update()
+    {
+        if (!lockAnswer)
+        {
+            if ((Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.E)) && cooldown <= Time.time)
+            {
+                if (DialogueQueue.Count > 0)
+                {
+                    textMesh.text = DialogueQueue.Dequeue();
+                    cooldown = Time.time + nextInputWaitTime;
+                }
+            }
+        }
+    }
+
+    public void SetInfo(string[] types, string[] names, string[][] features, int[] prices)
+    {
+        Types = types;
+        Names = names;
+        Features = features;
+        Prices = prices;
     }
 }
--- Vatan GameJam/Assets/Scripts/InfoCarrierr.cs
+++ Vatan GameJam/Assets/Scripts/InfoCarrierr.cs
@@ -1,23 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InfoCarrierr : MonoBehaviour
 {
-    public string[] Types;
-    public string[] Names;
-    public string[][] Features;
-    public int[] Prices;
-    // { get; private set; }
+
+    [Header("Options")]
+    [SerializeField] bool DestroyAfterUsage = true;
+
+    public string[] Types { get; private set; }
+    public string[] Names { get; private set; }
+    public string[][] Features { get; private set; }
+    public int[] Prices { get; private set; }
 
     public void SetInfo(ProductInfo[] Info)
     {
-        //for (int i = 0; i < Info.Length; i++)
-        //{
-        //    Types[i] = Info[i].ToString();
-        //    Names[i] = Info[i].productName;
-        //    Prices[i] = Info[i].productPrice;
-        //    Features[i] = Info[i].features;
-        //}
+        Types = new string[Info.Length];
+        Names = new string[Info.Length];
+        Prices = new int[Info.Length];
+        Features = new string[Info.Length][];
+
+        for (int i = 0; i < Info.Length; i++)
+        {
+            Types[i] = Info[i].productType.ToString();
+            Names[i] = Info[i].productName;
+            Prices[i] = Info[i].productPrice;
+            Features[i] = Info[i].features;
+        }
+
+        SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+    }
+
+    void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)//Sahne yüklendiði zaman
+    {
+        var HD = FindObjectOfType<HostDialogue>();
+        if (HD != null && (Types != null && Names != null && Features != null && Prices != null))
+        {
+            HD.SetInfo(Types, Names, Features, Prices);
+
+            if (DestroyAfterUsage)
+                Destroy(gameObject);
+        }
     }
 }
